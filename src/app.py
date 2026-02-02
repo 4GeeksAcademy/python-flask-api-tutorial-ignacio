4a92ed6c5e8fa42b71c049c63022237e848f6ea0
NachoRocchia <96082282+NachoRocchia@users.noreply.github.com>
2026-02-02 14:41:56 +0000

    delete request completo

--- src/app.py
+++ src/app.py
@@ -21,8 +21,10 @@ def add_new_todo():
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
     print("This is the position to delete:", position)
-    return 'something'
-
+    if position < 0 or position >= len(todos):
+        return jsonify({"error": "Invalid position"}), 404
+    del todos[position]
+    return jsonify(todos)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
